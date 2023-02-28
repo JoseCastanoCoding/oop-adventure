92b98e89d963ca3223e876d1c992f08545a358e2
JoseViewture <110395155+JoseViewture@users.noreply.github.com>
2023-02-28 11:22:40 +0000

    game finished!

---
+++ Source/Actions/Use.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace oop_adventure;
+
+public class Use : Action
+{
+    private readonly House _house; // this is the field!
+    
+    public Use(House house) // constructor, so that we can save a reference to the house that gets passed in when we make a new instance of the use class.
+    {
+        _house = house;
+    }
+
+    public override string Name => Text.Language.Use;
+
+    public override void Execute(string[] args)
+    {
+        if(args.Length < 1)
+        {
+            Console.WriteLine(Text.Language.UseError);
+
+            return;
+        }
+
+        var itemName = args[1];
+
+        if(_house.Player.Contains(itemName))
+        {
+            var item = _house.Player.Take(itemName);
+
+            Console.WriteLine(Text.Language.UseSuccess, item.Name);
+
+            item.Use(itemName);
+        }
+        else
+        {
+            Console.WriteLine(Text.Language.UseError);
+        }
+
+    }
+}
+
